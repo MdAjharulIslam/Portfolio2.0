f0a3bbafd2442568fefcda3dcd8c28a16a4698ae
ajharul <mdajharulislam327@gmail.com>
2025-03-10 16:12:37 +0600

    delete delay

--- assets/js/animated-headline.js
+++ assets/js/animated-headline.js
@@ -1,13 +1,13 @@
 jQuery(document).ready(function($){
 	//set animation timing
-	var animationDelay = 2500,
+	var animationDelay = 0,
 		//loading bar effect
-		barAnimationDelay = 3000,
-		barWaiting = barAnimationDelay - 1000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
+		barAnimationDelay = 0,
+		barWaiting = barAnimationDelay - 0, //3000 is the duration of the transition on the loading bar - set in the scss/css file
 		//letters effect
-		lettersDelay = 50,
+		lettersDelay = 0,
 		//type effect
-		typeLettersDelay = 150,
+		typeLettersDelay = 0,
 		selectionDuration = 500,
 		typeAnimationDelay = selectionDuration + 800,
 		//clip effect 
